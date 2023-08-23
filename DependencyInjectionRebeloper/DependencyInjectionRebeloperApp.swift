f554d25204a3c6ef9895c9f591d17a74b4f0f451
Marcin Jędrzejak <53284922+maartinj@users.noreply.github.com>
2023-08-24 00:06:19 +0200

    first commit: Load initial files

---
+++ DependencyInjectionRebeloper/DependencyInjectionRebeloperApp.swift
@@ -0,0 +1,17 @@
+//
+//  DependencyInjectionRebeloperApp.swift
+//  DependencyInjectionRebeloper
+//
+//  Created by Marcin Jędrzejak on 24/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct DependencyInjectionRebeloperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
